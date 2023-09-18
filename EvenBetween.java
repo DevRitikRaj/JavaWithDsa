7eb44f6181cb698c07f8e48102b4a31e4268200a
DevRitikRaj <rj1131999@gmail.com>
2023-09-18 22:24:46 +0530

    Second Program

---
+++ EvenBetween.java
@@ -0,0 +1,14 @@
+public class EvenBetween {
+    public static void main(String [] args){
+        // int a=9;
+        int z=100;
+        for(int a=9;a<z;a++){
+            if(a%2==0){
+                System.out.println(a);
+            }
+            
+
+        }
+    }
+    
+}
