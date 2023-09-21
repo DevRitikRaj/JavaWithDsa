604904f6e4e355c45f13505fd303ee380b684f41
DevRitikRaj <rj1131999@gmail.com>
2023-09-21 20:58:08 +0530

    Day 4

---
+++ TypeCasting/Conversion.java
@@ -0,0 +1,36 @@
+ /*Type Campatatible is means int convert into the float and float convert into the int or
+ int convert into boolean but boolean is not convert into int */
+
+
+package TypeCasting;
+
+
+public class Conversion {
+    public static void main(String [] args){
+        /*
+        float a=2.25f;
+        int b=(int) a;
+        System.out.println(b);
+
+        char ch='a';
+        int k=  ch;
+        System.out.println(k);
+
+
+        float num1=2.25f;
+        int con=(int) num1;
+        System.out.println(con);
+         */
+
+        //  byte l=5;
+        //  l=l*2;
+        //  System.out.println(l);
+
+        byte b=5;
+        b= (byte) b*2;
+        System.out.println(b);
+
+
+    }
+    
+}
