528a2f4fa23f72dd5dfe531b3fcc5352cfb00859
DevRitikRaj <rj1131999@gmail.com>
2023-09-28 23:40:52 +0530

    Day 10

---
+++ Loop/Revrese.java
@@ -0,0 +1,17 @@
+package Loop;
+import java.util.*;
+public class Revrese {
+    public static void main(String [] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n=sc.nextInt();
+        while(n>0){
+            int ld=n%10;
+            System.out.print(ld);
+            n/=10;
+        }
+        System.out.println();
+       
+    }
+    
+}
