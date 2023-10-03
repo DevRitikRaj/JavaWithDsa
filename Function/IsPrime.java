5efe4c89535a566c6c994c50a448d945d78fae26
DevRitikRaj <rj1131999@gmail.com>
2023-10-03 23:11:47 +0530

    Day 15

---
+++ Function/IsPrime.java
@@ -0,0 +1,23 @@
+package Function;
+
+public class IsPrime {
+    boolean prime =true;
+
+    public static boolean prime(int n){
+
+        if(n==2){
+            return true;
+        }
+        for(int i=2;i<=n-1;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String [] args){
+        System.out.println(prime(4));
+    }
+    
+}
