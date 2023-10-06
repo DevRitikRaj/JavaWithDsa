79088c7cca980cd18a6289e0ae8daba2209a0930
DevRitikRaj <rj1131999@gmail.com>
2023-10-06 22:43:07 +0530

    Day 18

---
+++ Adv_pattern/Rhombus_img.java
@@ -0,0 +1,24 @@
+package Adv_pattern;
+import java.util.*;
+public class Rhombus_img {
+
+    public static void SR(int n){
+        //Outer Loop
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Rhombus");
+        int n=sc.nextInt();
+        SR(n);
+    }
+    
+}
