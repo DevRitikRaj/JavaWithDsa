5370b1be599c61c18efb301d9b7cc04724eec612
DevRitikRaj <rj1131999@gmail.com>
2023-10-06 23:04:21 +0530

    Day 18

---
+++ Adv_pattern/Daimond.java
@@ -0,0 +1,41 @@
+package Adv_pattern;
+
+import java.util.Scanner;
+
+public class Daimond {
+    public static void daimomd(int n){
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            
+            System.out.println();
+           
+        }
+        for(int i=n;i>=1;i--){
+
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            
+            System.out.println();
+           
+        }
+
+    }
+    public static void main(String [] a){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of daimond shape ");
+        int n=sc.nextInt();
+        daimomd(5);
+
+    }
+    
+}
