4ab1e4e2e064ef270713e5baab1f2ebc06bf54e6
DevRitikRaj <rj1131999@gmail.com>
2023-10-21 22:46:50 +0530

    Day 31

---
+++ Arrays/Practice_02.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Practice_02 {
+
+    public static int Sell(int price[]){
+        int buyPrice=Integer.MAX_VALUE;
+        int maxProfit=0;
+
+        for(int i=0;i<price.length;i++){
+            if(buyPrice<price[i]){
+                int profit=price[i]-buyPrice;
+                maxProfit=Math.max(maxProfit, profit);
+            }else{
+                buyPrice=price[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String [] args){
+      Scanner sc= new Scanner(System.in);
+      System.out.println("Enter the size of ");
+      int n=sc.nextInt();
+      System.out.println("Enter the price of the stock");
+      int price[]=new int[n];
+      for(int i=0;i<n;i++){
+        price[i]=sc.nextInt();
+      }
+      System.out.println("The Maximum Stock "+Sell(price));
+    }
+    
+}
