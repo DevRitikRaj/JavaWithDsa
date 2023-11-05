3794e3552170562fe6de7461350c96318796f944
DevRitikRaj <rj1131999@gmail.com>
2023-11-05 15:36:43 +0530

    Day 40

---
+++ String/first.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.util.Scanner;
+
+public class first {
+    public static void main(String [] ad){
+        // String name="Ritik Raj";
+        // System.out.println(name);
+
+        // Scanner sc=new Scanner(System.in);
+        // String name;
+        // name=sc.next();
+
+        // // name=sc.nextLine();
+        // System.out.println(name);
+
+        //Contenation
+
+        String firstname="Ritik";
+        String lastname="Raj";
+        String fullname=(""+ firstname + ""+" "+lastname+"");
+        System.out.println(fullname);
+    }
+    
+}
