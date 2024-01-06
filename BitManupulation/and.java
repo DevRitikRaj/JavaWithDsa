736cd17f622164ad5ef3d0ae7b909a7dbc074132
DevRitikRaj <rj1131999@gmail.com>
2024-01-06 22:50:36 +0530

    shift

--- BitManupulation/and.java
+++ BitManupulation/and.java
@@ -2,8 +2,14 @@ package BitManupulation;
 
 public class and {
     public static void main(String [] args){
-        System.out.println(5 & 6);
-        
+        System.out.println(5 & 6); // & operator
+        System.out.println(5 | 6); // | or operator
+        System.out.println(5 ^ 6); // nor ^ operator
+        System.out.println(~5);
+        System.out.println(~0);
+
+
+
     }
     
 }
---
+++ BitManupulation/shift.java
@@ -0,0 +1,12 @@
+package BitManupulation;
+
+public class shift {
+
+    public static void main(String [] args)
+    {
+        int a=5;
+        int i=2;
+        System.out.println(a<<i);
+    }
+    
+}
