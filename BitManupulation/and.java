f9f2e1c38576a05fa11ce9c468276ea963ea9bfa
DevRitikRaj <rj1131999@gmail.com>
2024-01-07 23:07:11 +0530

    Update

--- BitManupulation/and.java
+++ BitManupulation/and.java
@@ -4,7 +4,7 @@ public class and {
     public static void main(String [] args){
         System.out.println(5 & 6); // & operator
         System.out.println(5 | 6); // | or operator
-        System.out.println(5 ^ 6); // nor ^ operator
+        System.out.println(5 ^ 5); // nor ^ operator
         System.out.println(~5);
         System.out.println(~0);
 
