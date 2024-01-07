63dcccb708826601aa139421db264e132716d57e
DevRitikRaj <rj1131999@gmail.com>
2024-01-07 21:38:32 +0530

    Update

---
+++ BitManupulation/Update.java
@@ -0,0 +1,24 @@
+package BitManupulation;
+
+public class Update {
+
+    public static int  clear(int n,int i){
+        int bitmask= ~(1<<i);
+        return n & bitmask;
+    }
+
+    public static int Upd(int n,int i, int newBit){
+        n=clear(n, i);
+        int bitmask=newBit<<i;
+        return n | bitmask;
+
+
+    }
+    public static void main(String [] args){
+        int n=10;
+        int i=2;
+        int newBit=1;
+        System.out.println(Upd(n, i, newBit));
+    }
+    
+}
