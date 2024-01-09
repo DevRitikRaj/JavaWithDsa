ea2c8873c5559620bd24c9089ba0617389d889e9
DevRitikRaj <rj1131999@gmail.com>
2024-01-09 19:43:52 +0530

    Opp2

--- Oops/Access.java
+++ Oops/Access.java
@@ -4,6 +4,11 @@ public class Access {
     public static void main(String[] args) {
 
         BankAccount b = new BankAccount();
+        b.username="Ritik";
+        b.setpassword("abcd");
+        System.out.println(b.username);
+        
+        // System.out.println(b.setpassword());
         
 
 
@@ -14,8 +19,14 @@ public class Access {
 class BankAccount {
     public String username;
     private String password;
+
+     public void setpassword(String pwd){
+        password=pwd;
+    }
+
+}
     
 
    
-    }
+    
 
