a0eee797973d2b27ea4cc0e694276a8b44e9abdd
DevRitikRaj <rj1131999@gmail.com>
2024-01-15 16:21:00 +0530

    Recursion

---
+++ Recursion/PrintNumber.java
@@ -0,0 +1,28 @@
+package Recursion;
+
+
+
+
+
+
+public interface PrintNumber {
+    public static void Number(int n){
+        if(n==1){
+            System.out.print(n);
+            return;
+        }
+        System.out.print(n+"  ");
+        Number(n-1);
+
+    
+
+    }
+    System.out.println();
+
+    public static void main(String [] args){
+        int n=10;
+        Number(n);
+
+    }
+    
+}
