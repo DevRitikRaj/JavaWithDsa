c985cda4978718f9de56f4e4f53be31da53df8bd
DevRitikRaj <rj1131999@gmail.com>
2024-01-28 23:24:59 +0530

    LeetCode_69

---
+++ Leetcode/question_69.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+public class question_69 {
+
+    public static double SquareRoot(int x){
+        return Math.sqrt(x);
+
+        
+    }
+
+    public static void main(String [] args){
+        System.out.println(SquareRoot(4));
+    }
+    
+
+
+}
