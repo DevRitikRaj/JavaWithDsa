99903f68845f99369eaf383d8eb533683c83aa88
DevRitikRaj <rj1131999@gmail.com>
2024-02-15 23:00:41 +0530

    Service Now

---
+++ ArrayList/Basic2.java
@@ -0,0 +1,63 @@
+package ArrayList;
+import java.util.ArrayList;
+
+public class Basic2 {
+    public static void main(String [] args){
+
+        // Syntax of the ArraysList
+        ArrayList <Integer> list =new ArrayList<>();
+
+      // Add the Element
+
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      list.add(4);
+      list.add(5);
+      list.add(6);
+      System.out.println(list);
+      System.out.println("--------------------");
+
+
+      System.out.println("Get Element");
+      list.get(2);
+      System.out.println(list);
+
+      System.out.println("--------------------------");
+
+      System.out.println("Get Remove");
+      list.remove(2);
+      System.out.println(list);
+      System.out.println("----------------------");
+
+
+      System.out.println("Set Element at index");
+
+      list.set(1, 55);
+      System.out.println(list);
+      System.out.println("-----------------");
+
+      System.out.println("Contianing / Available or not");
+
+      System.out.println(list.contains(1));
+      System.out.println(list.contains(22));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
