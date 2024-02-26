4bdd6c5066814177794e2abce871478eec80b560
DevRitikRaj <rj1131999@gmail.com>
2024-02-26 22:41:29 +0530

    Add the element

--- LinkedList/FirstPg.java
+++ LinkedList/FirstPg.java
@@ -13,12 +13,14 @@ public class FirstPg {
     }
     public static Node head;
     public static Node tail;
+    public static int size;
 
     // Adding will at the first position and last position
 
     public  void addFirst(int data){
         // step 1
         Node newNode =new Node(data);
+        size++;
         if(head==null){
             head=tail=newNode;
             return;
@@ -34,6 +36,7 @@ public class FirstPg {
     // Add last 
     public  void addlast(int data){
         Node newNode=new Node(data);
+        size++;
 
         if(head==null){
             head=tail=newNode;
@@ -64,6 +67,23 @@ public class FirstPg {
         System.out.println("null");
     }
 
+    public void add(int idx,int data){
+        if(idx==0){
+            addFirst(data);
+            return;
+        }
+        Node newNode=new Node(data);
+        size++;
+        Node temp=head;
+        int i=0;
+        while (i<idx-1) {
+            temp=temp.next;
+            i++;            
+        }
+        newNode.next=temp.next;
+        temp.next=newNode; // break the chain
+    }
+
     public static void main(String [] args){
         FirstPg ll =new FirstPg();
         ll.printLinkedlist();
@@ -74,7 +94,9 @@ public class FirstPg {
         ll.addlast(3);
         ll.printLinkedlist();
         ll.addlast(4);
+        ll.add(2, 8);
         ll.printLinkedlist();
+        System.out.println(ll.size);
 
 
 
